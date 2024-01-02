da3b9861fd960e0cc4fab5d56dc00c93a4bd4f97
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-01-01 21:42:49 -0500

    Create ZRect.java

---
+++ src/ZRect.java
@@ -0,0 +1,29 @@
+package src;
+
+class ZRect {
+    private int x, y;
+    private int width, height;
+
+    public ZRect(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
