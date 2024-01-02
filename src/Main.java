d936e0b6a3ba199acaf73fea1a0c431a7ce8bcdf
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-01-01 22:20:04 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ package src;
 
 import javax.swing.*;
 
+// The main class to run the game
 public class Main {
     public static void main(String[] args) {
+        // Create a JFrame for the game
         JFrame frame = new JFrame("Geometry Dash");
+
+        // Create an instance of the GeometryDash game
         GeometryDash game = new GeometryDash();
+
+        // Add the game to the frame
         frame.add(game);
+
+        // Set the frame size, visibility, and close operation
         frame.setSize(GeometryDash.WIDTH, GeometryDash.HEIGHT);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
