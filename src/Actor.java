f3d0fa8010d3bcf4a24e46b9f7f2cb34ab84faaf
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-01-01 21:42:29 -0500

    Create Actor.java

---
+++ src/Actor.java
@@ -0,0 +1,60 @@
+package src;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Actor {
+    private int x, y;
+    private int width, height;
+    private int xSpeed;
+    private Image image;
+
+    public Actor(String imageName, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = 3;
+
+        image = new ImageIcon(imageName).getImage();
+        this.width = image.getWidth(null);
+        this.height = image.getHeight(null);
+    }
+
+    public void update() {
+        x -= xSpeed;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(image, x, y, width, height, null);
+    }
+
+    public boolean collidesWith(Cube cube) {
+        return x < cube.getRight() &&
+                x + width > cube.getX() &&
+                y < cube.getBottom() &&
+                y + height > cube.getY();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getRight() {
+        return x + width;
+    }
+
+    public int getBottom() {
+        return y + height;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
