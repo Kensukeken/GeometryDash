4cf9821d88a839ec9d9116ee61dd7122320c1ae7
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-01-11 16:47:24 -0500

    Update Cube.java

--- src/Cube.java
+++ src/Cube.java
@@ -10,7 +10,7 @@ class Cube {
     private int x, y;
     private int width, height;
     private int ySpeed;
-
+    private double xSpeed;
     private int angle;
     private Image cubeImage;
     private ZRect floor;
@@ -24,6 +24,7 @@ class Cube {
         x = 50;
         y = 340 - height;
         ySpeed = 0;
+        xSpeed = 0;
         angle = 0;
 
         cubeImage = new ImageIcon("images/square.jpg").getImage();
@@ -32,7 +33,7 @@ class Cube {
 
     public void jump() {
         if (y + height >= floor.getY()) {
-            ySpeed = -15;
+            ySpeed = -20;
             isRotating = true;
         }
         y += ySpeed;
