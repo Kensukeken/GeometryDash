456af5050a4d6b6edca6446805f216c9a4fab391
Kensukeken <hiaxsaleh@gmail.com>
2024-01-15 14:53:47 -0500

    Update

--- src/SoundEffect.java
+++ src/SoundEffect.java
@@ -4,24 +4,29 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
 
-
-class SoundEffect{
+class SoundEffect {
     public Clip c;
-    public SoundEffect(String filename){
+
+    public SoundEffect(String filename) {
         setClip(filename);
     }
-    public void setClip(String filename){
-        try{
+
+    public void setClip(String filename) {
+        try {
             File f = new File(filename);
             c = AudioSystem.getClip();
             c.open(AudioSystem.getAudioInputStream(f));
-        } catch(Exception e){ System.out.println("error"); }
+        } catch (Exception e) {
+            System.out.println("error");
+        }
     }
-    public void play(){
+
+    public void play() {
         c.setFramePosition(0);
         c.start();
     }
-    public void stop(){
+
+    public void stop() {
         c.stop();
     }
 }
