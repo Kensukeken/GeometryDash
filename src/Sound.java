54aad5a1400e5f8994ccd9036553523ab51046cf
Kensukeken <hiaxsaleh@gmail.com>
2024-01-15 14:31:21 -0500

    Changes

--- src/Sound.java
+++ src/Sound.java
@@ -1,37 +1,35 @@
-package src; // Package for the src folder in IDEA editor
+package src;
 
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.sound.sampled.FloatControl;
 
-class Sound extends JFrame implements ActionListener
-{
+class Sound extends JFrame implements ActionListener {
     JButton btn = new JButton("Play Sound");
     SoundEffect sound;
-    private LinkedList<SoundEffect>sounds;
-    float volume=-30, dv=5;
+    private LinkedList<SoundEffect> sounds;
+    float volume = -30, dv = 5;
 
-    public Sound()
-    {
-        setSize(300,100);
-        setLocation(400,300);
+    public Sound() {
+        setSize(300, 100);
+        setLocation(400, 300);
         JPanel jp = new JPanel();
         btn.addActionListener(this);
         jp.add(btn);
         add(jp);
         pack();
-        for(int i =0; i<10; i++){
-            sounds.add(new SoundEffect("stero-madmess-geometry-dash.wav"));
+        for (int i = 0; i < 10; i++) {
+            sounds.add(new SoundEffect("stereo-madness-geometry-dash"));
         }
     }
 
-    public void actionPerformed(ActionEvent ae){
+    public void actionPerformed(ActionEvent ae) {
         SoundEffect sound = sounds.get(0);
         FloatControl gainControl = (FloatControl) sound.c.getControl(FloatControl.Type.MASTER_GAIN);
         volume += dv;
-        if(volume==-30 || volume == 5){
-            dv*=-1;
+        if (volume == -30 || volume == 5) {
+            dv *= -1;
         }
         gainControl.setValue(volume);
         System.out.println(volume);
@@ -40,7 +38,8 @@ class Sound extends JFrame implements ActionListener
         sounds.remove(0);
         sounds.add(sound);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         new Sound().setVisible(true);
     }
 }
